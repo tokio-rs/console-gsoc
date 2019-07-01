580f594aa690dc29155fb476a39504c928873729
Matthias Prechtl <m.sleepypanda@gmail.com>
2019-07-01 17:35:12 +0200

    Tui refactor (#24)
    * Store thread names
    
    * Implement demo UI, two threads + selector, two log scenarios
    
    * Show message selection/position
    
    * Move thread selector into own module
    
    * rustfmt + unused

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,106 +1,45 @@
-use storage::{Store, ThreadId};
+use storage::Store;
 
 use tokio_trace::Level;
 use tui::backend::CrosstermBackend;
-use tui::layout::{Constraint, Direction, Layout, Rect};
-use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, List, SelectableList, Text, Widget};
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Style};
+use tui::widgets::{Block, Borders, List, Text, Widget};
 use tui::Frame;
 
 use crossterm::{InputEvent, KeyEvent};
 
-use std::cell::Cell;
+use crate::ui::ThreadSelector;
+
 use std::sync::{Arc, Mutex};
 
 pub struct App {
     store: Arc<Mutex<Store>>,
-    current_thread: Cell<Option<ThreadId>>,
+    thread_selector: ThreadSelector,
 }
 
 impl App {
     pub fn new(store: Arc<Mutex<Store>>) -> App {
         App {
             store,
-            current_thread: Cell::new(None),
+            thread_selector: ThreadSelector::new(),
         }
     }
 
-    fn on_up(&mut self) {
-        // TODO: Cache UI elements and access cache
+    pub fn update(&mut self) -> bool {
         let store = self.store.lock().unwrap();
-        if let Some(current_id) = self.current_thread.get() {
-            let mut threads = store
-                .threads
-                .iter()
-                .map(|(key, store)| (key, &store.name))
-                .collect::<Vec<(&ThreadId, &Option<String>)>>();
-            threads.sort_by_key(|(&id, _)| id);
-            let current_index = threads
-                .iter()
-                .position(|(id, _)| current_id == **id)
-                .expect("BUG: Current thread id not in list");
-            if let Some((id, _)) = threads.get(current_index.saturating_sub(1)) {
-                self.current_thread.set(Some(**id))
-            }
+        if store.updated() {
+            self.thread_selector.update(&store)
+        } else {
+            false
         }
     }
 
-    fn on_down(&mut self) {
-        // TODO: Cache UI elements and access cache
-        let store = self.store.lock().unwrap();
-        if let Some(current_id) = self.current_thread.get() {
-            let mut threads = store
-                .threads
-                .iter()
-                .map(|(key, store)| (key, &store.name))
-                .collect::<Vec<(&ThreadId, &Option<String>)>>();
-            threads.sort_by_key(|(&id, _)| id);
-            let current_index = threads
-                .iter()
-                .position(|(id, _)| current_id == **id)
-                .expect("BUG: Current thread id not in list");
-            if let Some((id, _)) = threads.get(current_index.saturating_add(1)) {
-                self.current_thread.set(Some(**id))
-            }
-        }
+    fn on_up(&mut self) -> bool {
+        self.thread_selector.on_up()
     }
-
-    fn thread_selector(&self, store: &Store, f: &mut Frame<CrosstermBackend>, r: Rect) {
-        // TODO: Cache UI elements
-        let mut threads = store
-            .threads
-            .iter()
-            .map(|(key, store)| (key, &store.name))
-            .collect::<Vec<(&ThreadId, &Option<String>)>>();
-        threads.sort_by_key(|(&id, _)| id);
-        let index = self.current_thread.get().and_then(|current_id| {
-            threads.iter().enumerate().find_map(
-                |(i, (id, _))| {
-                    if current_id == **id {
-                        Some(i)
-                    } else {
-                        None
-                    }
-                },
-            )
-        });
-        SelectableList::default()
-            .highlight_style(Style::default().modifier(Modifier::BOLD))
-            .items(
-                &threads
-                    .iter()
-                    .map(|(key, name)| {
-                        format!(
-                            "{} - {}",
-                            key.0,
-                            if let Some(name) = name { name } else { "" }
-                        )
-                    })
-                    .collect::<Vec<String>>(),
-            )
-            .select(index)
-            .block(Block::default().borders(Borders::ALL).title("Threads"))
-            .render(f, r);
+    fn on_down(&mut self) -> bool {
+        self.thread_selector.on_down()
     }
 
     /// Returns if the scene has to be redrawn
@@ -108,14 +47,8 @@ impl App {
         let redraw = match event {
             InputEvent::Keyboard(key) => match key {
                 KeyEvent::Char('q') => return None,
-                KeyEvent::Up => {
-                    self.on_up();
-                    true
-                }
-                KeyEvent::Down => {
-                    self.on_down();
-                    true
-                }
+                KeyEvent::Up => self.on_up(),
+                KeyEvent::Down => self.on_down(),
                 _ => false,
             },
             _ => false,
@@ -125,49 +58,38 @@ impl App {
 
     pub fn render_to(&self, f: &mut Frame<CrosstermBackend>) {
         let store = self.store.lock().unwrap();
-        if self.current_thread.get().is_none() {
-            if store.threads.len() == 1 {
-                // There is at least one element
-                let thread_id = store.threads.keys().next().unwrap();
-                self.current_thread.set(Some(*thread_id));
-            } else {
-                if let Some(thread_id) = store.threads.keys().min() {
-                    self.current_thread.set(Some(*thread_id));
-                }
-            }
-        } else {
-            if !store.updated() {
-                return;
-            }
-        }
-
         let chunks = Layout::default()
             .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
             .direction(Direction::Horizontal)
             .split(f.size());
 
-        self.thread_selector(&store, f, chunks[0]);
-        if let Some(current_thread) = &self.current_thread.get() {
-            let logs = store
+        self.thread_selector.render_to(f, chunks[0]);
+        if let Some(current_thread) = &self.thread_selector.current_thread() {
+            let lines = &store
                 .threads
                 .get(current_thread)
                 .expect("BUG: No logs for the thread")
-                .lines
-                .iter()
-                .map(|(level, text)| {
-                    Text::styled(
-                        text,
-                        match *level {
-                            Level::INFO => Style::default().fg(Color::White),
-                            Level::ERROR => Style::default().fg(Color::Red),
-                            Level::WARN => Style::default().fg(Color::LightRed),
-                            Level::TRACE => Style::default().fg(Color::Cyan),
-                            Level::DEBUG => Style::default().fg(Color::LightMagenta),
-                        },
-                    )
-                });
+                .lines;
+            let len = lines.len();
+            let logs = lines.iter().map(|(level, text)| {
+                Text::styled(
+                    text,
+                    match *level {
+                        Level::INFO => Style::default().fg(Color::White),
+                        Level::ERROR => Style::default().fg(Color::Red),
+                        Level::WARN => Style::default().fg(Color::LightRed),
+                        Level::TRACE => Style::default().fg(Color::Cyan),
+                        Level::DEBUG => Style::default().fg(Color::LightMagenta),
+                    },
+                )
+            });
             List::new(logs)
-                .block(Block::default().borders(Borders::ALL).title("Messages"))
+                .block(Block::default().borders(Borders::ALL).title(&format!(
+                    "Messages {}-{}/{} ",
+                    1,
+                    std::cmp::min((chunks[1].height - 2) as usize, len),
+                    len
+                )))
                 .render(f, chunks[1]);
         } else {
             let logs = vec![Text::raw("--- No Messages ---")].into_iter();
---
+++ src/ui/thread.rs
@@ -0,0 +1,112 @@
+use storage::{Store, ThreadId};
+use tui::backend::CrosstermBackend;
+use tui::layout::Rect;
+use tui::style::{Modifier, Style};
+use tui::widgets::{Block, Borders, SelectableList, Widget};
+use tui::Frame;
+
+pub struct ThreadSelector {
+    current_thread: Option<ThreadId>,
+    threads: Vec<(ThreadId, Option<String>)>,
+}
+
+impl ThreadSelector {
+    pub(crate) fn new() -> ThreadSelector {
+        ThreadSelector {
+            current_thread: None,
+            threads: Vec::new(),
+        }
+    }
+
+    pub(crate) fn current_thread(&self) -> Option<ThreadId> {
+        self.current_thread
+    }
+
+    pub(crate) fn update(&mut self, store: &Store) -> bool {
+        let mut threads = store
+            .threads
+            .iter()
+            .map(|(key, store)| (*key, store.name.clone()))
+            .collect::<Vec<(ThreadId, Option<String>)>>();
+        threads.sort_by_key(|(id, _)| *id);
+        let rerender = self.threads == threads;
+        self.threads = threads;
+        if self.current_thread.is_none() {
+            if self.threads.len() == 1 {
+                // There is at least one element
+                let thread_id = self.threads.iter().map(|(id, _)| id).next().unwrap();
+                self.current_thread = Some(*thread_id);
+            } else {
+                if let Some(thread_id) = self.threads.iter().map(|(id, _)| id).min() {
+                    self.current_thread = Some(*thread_id);
+                }
+            }
+        }
+        rerender
+    }
+
+    pub(crate) fn on_up(&mut self) -> bool {
+        if let Some(current_id) = self.current_thread {
+            let current_index = self
+                .threads
+                .iter()
+                .position(|(id, _)| current_id == *id)
+                .expect("BUG: Current thread id not in list");
+            if let Some((id, _)) = self.threads.get(current_index.saturating_sub(1)) {
+                let new_id = Some(*id);
+                let rerender = self.current_thread != new_id;
+                self.current_thread = new_id;
+                return rerender;
+            }
+        }
+        false
+    }
+
+    pub(crate) fn on_down(&mut self) -> bool {
+        if let Some(current_id) = self.current_thread {
+            let current_index = self
+                .threads
+                .iter()
+                .position(|(id, _)| current_id == *id)
+                .expect("BUG: Current thread id not in list");
+            if let Some((id, _)) = self.threads.get(current_index.saturating_add(1)) {
+                let new_id = Some(*id);
+                let rerender = self.current_thread != new_id;
+                self.current_thread = new_id;
+                return rerender;
+            }
+        }
+        false
+    }
+
+    pub(crate) fn render_to(&self, f: &mut Frame<CrosstermBackend>, r: Rect) {
+        let index =
+            self.current_thread.and_then(|current_id| {
+                self.threads.iter().enumerate().find_map(|(i, (id, _))| {
+                    if current_id == *id {
+                        Some(i)
+                    } else {
+                        None
+                    }
+                })
+            });
+        SelectableList::default()
+            .highlight_style(Style::default().modifier(Modifier::BOLD))
+            .items(
+                &self
+                    .threads
+                    .iter()
+                    .map(|(key, name)| {
+                        format!(
+                            "{} - {}",
+                            key.0,
+                            if let Some(name) = name { name } else { "" }
+                        )
+                    })
+                    .collect::<Vec<String>>(),
+            )
+            .select(index)
+            .block(Block::default().borders(Borders::ALL).title("Threads"))
+            .render(f, r);
+    }
+}
